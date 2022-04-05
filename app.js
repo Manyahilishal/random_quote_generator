33800c0fc638425f52be2cd4aab9df7f8934f9ca
Manny <manyaabat@gmail.com>
2022-04-04 20:18:10 -0400

    files updated

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("connected")
+let quote = [" “We become what we think about” – Earl Nightingale", "I am Wolyta, Author Manny Dado", "life is better with family, Manny", "sharing is caring, Steph", "we are bonded for life, Carmalin"]
+let randomNumber = Math.floor(Math.random() *3);
+function newQuote(){
+    let randomNumber =Math.floor(Math.random() *(quote.length));
+    document.getElementById('qouteDisplay').innerHTML = quote[randomNumber]
+} 
